067fd21b2b46960696d7fb1410ffff080cdc1ccc
Jay Caines-Gooby <jay@gooby.org>
2009-03-31 15:32:54 +0100

    Actually useful for redirects now, as it does more than just alias one internal route to another. Can also redirect from /foo to http://bar.com for instance

--- thoth_redirect.rb
+++ thoth_redirect.rb
@@ -22,6 +22,33 @@ module Thoth; module Plugin
       # using rewrite should then allow us to use regex in redirects.yml
       # as long as we convert everything to a Regexp first
       Ramaze::Route[ Regexp.new(original_location)] = redirected_location
-    end
+    end 
+    
+    module ::Ramaze; class Controller; class << self
+      
+      attr_accessor :last_path
+      
+      # method chain aliasing - don't blow away Ramaze::Controller.raise_no_action
+      alias :original_raise_no_action :raise_no_action
+                    
+      def raise_no_action(controller, path)
+        # just handle this the normal Ramaze way if this is an unknown action and we haven't got a redirect defined for it
+        # we also trap any redirects that result in a circular set of redirects:
+        # /foo redirects to /bar but there's no action associated with /bar so repeat the process again and again
+        # Some browsers would show a "too many redirects" error, but its better just to trap this here instead and 404 (or whatever) it.
+                
+        if ! Ramaze::Route.trait[:routes].values.include?(path) or path == @last_path
+          original_raise_no_action(controller, path) 
+        else
+          # otherwise it's actually one of our redirects and yes, we don't have an action associated with it or we wouldn't be
+          # here, so try an actual redirect
+          @last_path = path
+          raw_redirect(path, :status => 301)                
+        end
+      end    
+      
+    end; end; end; #::Ramaze::Controller
+  
   end;
+  
 end; end
